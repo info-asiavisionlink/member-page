aea669b9361e7ec45e963f29f2a497f2c8d884ad
info-asiavisionlink <info@asiavision.link>
2026-02-12 00:14:57 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "NEXT ASIA LINK | Member",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
